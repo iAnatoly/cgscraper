d52ae12f4241f8e35fd0752a398e9c1adf792ab6
Anatoly Ivanov <anatoly.ivanov@gmail.com>
2017-06-04 10:32:43 -0700

    minor refactoring

--- scraper.py
+++ scraper.py
@@ -11,90 +11,105 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+class Scraper:
+	def __init__(self,config):
+		self.config = config
 
-def sendEmail(result):
-	with open('config.json') as json_data_file:
-    		config = json.load(json_data_file)
+	def send_email(self, result):
 
-	recipients = config['recipients']
-	subject = config['subject']
-	mail_from = config['sender']
-	smtpserver = config['smtp']['server']
-	smtpuser = config['smtp']['user']
-	smtppass = config['smtp']['pass']
+		recipients = self.config['email']['recipients']
+		subject = self.config['email']['subject']
+		mail_from = self.config['email']['sender']
+		smtpserver = self.config['email']['smtp']['server']
+		smtpuser = self.config['email']['smtp']['user']
+		smtppass = self.config['email']['smtp']['pass']
 
-        try:
-            server = smtplib.SMTP(smtpserver)
-            server.set_debuglevel(False)
-            server.ehlo()
-            server.starttls()
-            server.login(smtpuser, smtppass)
+		try:
+		    server = smtplib.SMTP(smtpserver)
+		    server.set_debuglevel(False)
+		    server.ehlo()
+		    server.starttls()
+		    server.login(smtpuser, smtppass)
 
-            msg = MIMEText(result, 'plain')
-            msg['Subject'] = subject
-            msg['From'] = 'Calguns Scraper <{}>'.format(mail_from)
-            msg['To'] = ';'.join(recipients)
+		    msg = MIMEText(result, 'plain')
+		    msg['Subject'] = subject
+		    msg['From'] = 'Calguns Scraper <{}>'.format(mail_from)
+		    msg['To'] = ';'.join(recipients)
 
-            server.sendmail(mail_from, recipients, msg.as_string())
+		    server.sendmail(mail_from, recipients, msg.as_string())
 
-            server.quit()
+		    server.quit()
 
-        except Exception as e:
-            print "\nError sending out email: {0}.".format(e)
-	    raise e
-
-first = None
-with open('scraper.last') as f:
-	last = f.readline().strip()
-
-print('last: "{}"'.format(last))
-
-url = 'https://www.calguns.net/calgunforum/forumdisplay.php?f=332&order=desc&page={}'
-targets = [
-	re.compile('cz.*shadow', flags=re.IGNORECASE), 
-	re.compile('tactical\s+sport', flags=re.IGNORECASE), 
-	re.compile('tacsport', flags=re.IGNORECASE), 
-	re.compile('cz.*75.*ts', flags=re.IGNORECASE),
-]
-
-pagenum = 1
-while(True):
-	print('Fetching page {}'.format(pagenum))
-	page = requests.get(url.format(pagenum))
-	content = page.content 
-	tree = html.fromstring(content)
-
-	postings = tree.xpath('//td[starts-with(@id,"td_threadtitle_")]')
+		except Exception as e:
+		    print "\nError sending out email: {0}.".format(e)
+		    raise e
 	
-	if not postings:
-		print("no more posts")
-		break
+	def fetch_content(self, pagenum):
+		print('Fetching page {}'.format(pagenum))
+		url_template = self.config['url_template']
+		url = url_template.format(pagenum)
+		page = requests.get(url)
+		return page.content 
 
-	for posting in postings:
-		id = posting.get('id')
-		title = posting.get('title').replace('\n','')
+	def scrape(self):
+		first = None
+		with open('scraper.last') as f:
+			last = f.readline().strip()
 
-		if id == last:
-			print("done")
-			break
+		print('last: "{}"'.format(last))
 
-		for regex in targets:
-			if regex.search(title):
-				print('*** MATCH ***')
-				print(title)
-				sendEmail(title)
+		targets = []
+		for target in self.config['scraping_targets']:
+			targets.append(re.compile(target, flags=re.IGNORECASE))
 
-		if not first and title.startswith('Make:'):
-			first = id			
+		pagenum = 1
 
-	
-	if id == last:
-		print("and done")
-		break
+		while(True):
 
-	pagenum += 1
+			content = self.fetch_content(pagenum)
+			tree = html.fromstring(content)
 
-if first:
-	print('saving {} as current position'.format(first))
-	with open('scraper.last','w') as f:
-		f.write(first)
+			postings = tree.xpath('//td[starts-with(@id,"td_threadtitle_")]')
+			
+			if not postings:
+				print("no more posts")
+				break
+
+			for posting in postings:
+				id = posting.get('id')
+				title = posting.get('title').replace('\n','')
+
+				if id == last:
+					print("done")
+					break
+
+				for regex in targets:
+					if regex.search(title):
+						print('*** MATCH ***')
+						print(title)
+						self.send_email(title)
+
+				if not first and title.startswith('Make:'):
+					first = id			
+
+			
+			if id == last:
+				print("and done")
+				break
+
+			pagenum += 1
+
+		if first:
+			print('saving {} as current position'.format(first))
+			with open('scraper.last','w') as f:
+				f.write(first)
+
+def main():
+	with open('scraper.config.json') as json_data_file:
+		config = json.load(json_data_file)
+
+	scraper = Scraper(config)
+	scraper.scrape()	
+
+
+main()
