0871296078ee2c6d2310b88ca1385d13e644270c
Anatoly Ivanov <anatoly.ivanov@gmail.com>
2017-06-04 14:58:34 -0700

    forgot setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='scraper',
+      version='1.0',
+      description='scraper app',
+      author='Anatoly',
+      author_email='anatoly.ivanov@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['requests', 'lxml'],
+      )
